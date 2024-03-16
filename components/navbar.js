52c8928222034790de003e0815c4e7b63f196d63
hello-jetlearn <162784982+hello-jetlearn@users.noreply.github.com>
2024-03-16 17:41:11 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+const navbar = () => {
+    return `<div class="nav">
+    <div class="logo"><a href="./index.html"><img src="./mainlogo2.png" alt="food app"></a></div>
+
+    <ul>
+       
+        <li><a href="wallet.html">Wallet</a></li>
+        <li><a href="cart.html">Cart</a><sup></sup></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="signup.html">SignUp</a></li>
+        <li><a href="https://docs.google.com/forms/d/1dJa3OBXrmrnVVFYhRDNP2HTVxWDIoMHy20qOrSIOPBA/prefill">CheckOut</a></li>
+    </ul>
+</div>`
+
+}
+
+export default navbar;
+
